25f01b3fc34e8ccc7cf0c322cdcf4bc9fba9ac0d
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2022-11-09 18:01:25 +0530

    Update Testimonial.js

--- sections/common/Testimonial.js
+++ sections/common/Testimonial.js
@@ -40,7 +40,7 @@ const Testimonial = ({ testimonialArr }) => {
                                                     <SwiperSlide>
                                                         <Box className="d-flex">
                                                             <Box className="flex-shrink-0">
-                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important" }} />
+                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important",marginTop:"-3rem !important" }} />
                                                             </Box>
                                                             <Box className="flex-grow-1 ms-3">
                                                                 <Typography style={{ fontSize: "1.1rem !important" }}><strong>They carried out a challenging replacement of our failing AC system in the middle of a horrible heat wave.  They are courteous and very knowledgeable about their job. Highly recommend them.</strong></Typography>
@@ -51,7 +51,7 @@ const Testimonial = ({ testimonialArr }) => {
                                                     <SwiperSlide>
                                                         <Box className="d-flex">
                                                             <Box className="flex-shrink-0 mt-n5">
-                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important" }} />
+                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important",marginTop:"-3rem !important" }} />
                                                             </Box>
                                                             <Box className="flex-grow-1 ms-3">
                                                                 <Typography style={{ fontSize: "1.1rem !important" }}><strong>The entire Teressen Heating & Air Conditioning team has been great to work with. They protected our floors and working area throughout the day, provided a clear understanding of the work, and left the job site in the same way as they started (very clean!). We have been happy to be a customer of Teressen.</strong></Typography>
@@ -62,7 +62,7 @@ const Testimonial = ({ testimonialArr }) => {
                                                     <SwiperSlide>
                                                         <Box className="d-flex">
                                                             <Box className="flex-shrink-0 mt-n5">
-                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important" }} />
+                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important",marginTop:"-3rem !important" }} />
                                                             </Box>
                                                             <Box className="flex-grow-1 ms-3">
                                                                 <Typography style={{ fontSize: "1.1rem !important" }}><strong>I've always found their technicians are very tentative, they let me know if they see any issues or concerns about the unit. It was perfect! They were on time, very genuine & professional!</strong></Typography>
@@ -73,7 +73,7 @@ const Testimonial = ({ testimonialArr }) => {
                                                     <SwiperSlide>
                                                         <Box className="d-flex">
                                                             <Box className="flex-shrink-0 mt-n5">
-                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important" }} />
+                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important",marginTop:"-3rem !important" }} />
                                                             </Box>
                                                             <Box className="flex-grow-1 ms-3">
                                                                 <Typography style={{ fontSize: "1.1rem !important" }}><strong>I want to thank you for your excellent service. They answered all my questions and was very professional. He gave me peace of mind and complete comfort. I know I am in good hands with your company.</strong></Typography>
@@ -84,7 +84,7 @@ const Testimonial = ({ testimonialArr }) => {
                                                     <SwiperSlide>
                                                         <Box className="d-flex">
                                                             <Box className="flex-shrink-0 mt-n5">
-                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important" }} />
+                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important",marginTop:"-3rem !important" }} />
                                                             </Box>
                                                             <Box className="flex-grow-1 ms-3">
                                                                 <Typography style={{ fontSize: "1.1rem !important" }}><strong>Teressen is the epitome of excellence in customer service and professionalism. They explained the process of repairing my A/C every step of the way and gave me an estimate before each action. I understand my unit better due to their detailed explanation of how the whole system operates and I truly appreciated their maintenance suggestions to extend the life of my teams. Excellent job! You have a loyal customer here!</strong></Typography>
@@ -95,7 +95,7 @@ const Testimonial = ({ testimonialArr }) => {
                                                     <SwiperSlide>
                                                         <Box className="d-flex">
                                                             <Box className="flex-shrink-0 mt-n5">
-                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important" }} />
+                                                                <FormatQuote className='text--blue' style={{ fontSize: "5rem", color: "var(--blue) !important",marginTop:"-3rem !important" }} />
                                                             </Box>
                                                             <Box className="flex-grow-1 ms-3">
                                                                 <Typography style={{ fontSize: "1.1rem !important" }}><strong>They showed up when they said they would and were on time. Professional and extremely polite. There was leak in our system and they repaired the leak and had the AC freezing us out in no time. I recommend them.</strong></Typography>
